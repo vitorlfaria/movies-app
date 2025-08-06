a1044cd829b649cf346f677ac904372d8f5e2c64
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2025-08-05 21:14:53 -0300

    feat: implement search functionality and enhance movie fetching logic with API integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,36 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
+import SearchBar from "../../components/SearchBar";
+import { icons } from "../../constants/icons";
+import { images } from "../../constants/images";
 
 export default function Index() {
+	const router = useRouter();
+
 	return (
-		<View className="flex-1 items-center justify-center">
-			<Text className="text-5xl text-blue-500 font-bold">Welcome!</Text>
+		<View className="flex-1 bg-primary">
+			<Image source={images.bg} className="absolute w-full z-0" />
+
+			<ScrollView
+				className="flex-1 px-5"
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{
+					minHeight: "100%",
+					paddingBottom: 10,
+				}}
+			>
+				<Image
+					source={icons.logo}
+					className="w-12 h-10 mt-20 mb-5 mx-auto"
+				/>
+
+				<View className="flex-1 mt-5">
+					<SearchBar
+						onPress={() => router.push("/search")}
+						placeholder="Search for a movie"
+					/>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
